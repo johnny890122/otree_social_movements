8c9b336f9677bb3c8dc0a2a8e78bc53dcbdcd66f
Xiang Xian, Zhang <j087061439@gmail.com>
2025-02-27 12:51:13 +0800

    feat: 完成頁面邏輯以及遊戲示範範例

--- social_movements/__init__.py
+++ social_movements/__init__.py
@@ -1,16 +1,25 @@
-from otree.api import BasePlayer, BaseSubsession, BaseConstants, BaseGroup, Page, WaitPage, models
+from otree.api import BasePlayer, BaseSubsession, BaseConstants, BaseGroup, Page, WaitPage, models, widgets
 import json, itertools
 class C(BaseConstants):
     NAME_IN_URL = 'css'
     PLAYERS_PER_GROUP = 4
     NUM_ROUNDS = 10
     NETWORKS = ["type_I", "type_II"]
+    MAPPING = {i: chr(64 + i) for i in range(1, 5)}  # Dynamically generate 'A', 'B', 'C', 'D'
 
 class Subsession(BaseSubsession):
     pass
 
 class Player(BasePlayer):
-    pass
+    threshold = models.IntegerField(
+        label="Please enter a threshold value",
+        choices=[1, 2, 3, 4],
+    )
+
+    revolt = models.BooleanField(
+        label="Do you want to revolt?",
+        choices=[True, False],
+    )
 
 class Group(BaseGroup):
     treatment = models.StringField()
@@ -71,10 +80,8 @@ class RulePhase2Page(Page):
         with open(network_file, "r") as f:
             data = json.load(f)
 
-        mapping = {i: chr(64 + i) for i in range(1, 5)}  # Dynamically generate 'A', 'B', 'C', 'D'
-
         return {
-            "me": json.dumps(mapping.get(player.id_in_group, "Unknown")),
+            "me": json.dumps(C.MAPPING.get(player.id_in_group, "Unknown")),
             "nodes": json.dumps(data.get("nodes", [])),
             "links": json.dumps(data.get("links", [])),
         }
@@ -92,37 +99,66 @@ class RulePhase4Page(Page):
         if player.round_number == 1:
             return True
         return False
+    
+    @staticmethod
+    def vars_for_template(player):
+        rewards_file = "_static/rewards.json"
+        with open(rewards_file, "r") as f:
+            data = json.load(f)
+        
+        return {
+            "rewards": data,
+            # TODO: Set up the example data
+            "revolt_outcome": json.dumps(True),
+            "num_participants": "3",
+            "rewards": 100,
+        }
 
 class Phase1Page(Page):
+    form_model = 'player'
+    form_fields = ['threshold']
+
     @staticmethod
     def is_displayed(player):
-        pass
+        return True
 
     @staticmethod
     def vars_for_template(player):
-        pass
+        return {
+            "round_number": player.round_number
+        }
 
     @staticmethod
     def before_next_page(player, timeout_happened):
+        # TODO: Implement the logic for the threshold
         pass
 
 class Phase2Page(Page):
     @staticmethod
     def is_displayed(player):
-        pass
+        return True
 
     @staticmethod
     def vars_for_template(player):
-        pass
+        network_file = f"_static/networks/{player.session.config['network']}.json"
 
-    @staticmethod
-    def before_next_page(player, timeout_happened):
-        pass
+        with open(network_file, "r") as f:
+            data = json.load(f)
+
+        return {
+            "round_number": player.round_number,
+            "me": json.dumps(C.MAPPING.get(player.id_in_group, "Unknown")),
+            "nodes": json.dumps(data.get("nodes", [])),
+            "links": json.dumps(data.get("links", [])),
+        }
 
 class Phase3Page(Page):
+    form_model = 'player'
+    form_fields = ['revolt']
+
     @staticmethod
     def is_displayed(player):
-        pass
+        return True
 
     @staticmethod
     def vars_for_template(player):
@@ -131,20 +167,28 @@ class Phase3Page(Page):
     @staticmethod
     def before_next_page(player, timeout_happened):
         pass
+        # TODO: Calculate the outecome here
 
 class Phase4Page(Page):
     @staticmethod
     def is_displayed(player):
-        pass
+        return True
 
     @staticmethod
     def vars_for_template(player):
-        pass
+        rewards_file = "_static/rewards.json"
+        with open(rewards_file, "r") as f:
+            data = json.load(f)
+        
+        return {
+            "rewards": data,
 
-    @staticmethod
-    def before_next_page(player, timeout_happened):
-        pass
-
+            # TODO: Replace the example data
+            "round_number": player.round_number,
+            "revolt_outcome": json.dumps(True),
+            "num_participants": "3",
+            "rewards": 100,
+        }
 
 class ArrivalPage(WaitPage):
     # group_by_arrival_time = True
@@ -154,4 +198,5 @@ class ArrivalPage(WaitPage):
         return player.round_number == 1
 
 # page_sequence = [ArrivalPage, WelcomePage, IntroPage, RulePhase1Page, RulePhase2Page, RulePhase3Page, RulePhase4Page, ArrivalPage]
-page_sequence = [WelcomePage, IntroPage, RulePhase1Page, RulePhase2Page, RulePhase3Page, RulePhase4Page]
+# page_sequence = [WelcomePage, IntroPage, RulePhase1Page, RulePhase2Page, RulePhase3Page, RulePhase4Page]
+page_sequence = [Phase1Page, Phase2Page, Phase3Page, Phase4Page]
